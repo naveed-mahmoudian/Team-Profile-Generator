fa4dad84718a083784df3ad49de400cf04df4ff8
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-10-22 16:03:18 -0700

    cleaned up code in index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // Import classes
-const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
